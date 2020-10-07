9fb77f9e1af0ff9a61968a60558c1d317302c604
Victtor <victtorfreitas95@gmail.com>
2020-10-06 22:11:06 -0300

    Inicializa projeto

---
+++ src/main/java/br/com/navita/navita/NavitaApplication.java
@@ -0,0 +1,13 @@
+package br.com.navita.navita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/navita/navita/NavitaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.navita.navita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
